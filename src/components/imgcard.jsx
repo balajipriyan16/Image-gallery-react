c3b37e445172a71a836c6cdb40fefac7b387fcb4
Balaaji Priyan A P <balajipriyan162005@gmail.com>
2026-07-25 15:05:04 +0530

    Update image source with compression and dynamic title

--- src/components/imgcard.jsx
+++ src/components/imgcard.jsx
@@ -2,7 +2,7 @@
 function Imgcards(data){
     return(
         <div className="Imgcard">
-            <img src={data.Cimage} alt="Cars" />
+            <img src={`${data.Cimage}?auto=compress&cs=tinysrgb&w=400`} alt={data.Cname} title={data.Cname}/>
             <p>{data.Cname}</p>
             <p><strong>Brand: </strong>{data.Ccompany}</p>
             <p><strong>Year: </strong>{data.Cyear}</p>
